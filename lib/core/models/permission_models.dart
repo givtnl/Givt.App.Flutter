7c7134d28e611b6a2fbf6caeb505f4ef8bfb3207
Daniela510 <whateverellie@gmail.com>
2022-09-28 13:10:55 +0200

    file restructuring

---
+++ lib/core/models/permission_models.dart
@@ -0,0 +1,11 @@
+enum LocationSelection {
+  noLocationPermission, // Permission denied, but not forever
+  noLocationPermissionPermanent, // Permission denied forever
+  yesLocationAccess //Yay
+}
+
+enum CameraSelection {
+  noCameraPermission, // Permission denied, but not forever
+  noCameraPermissionPermanent, // Permission denied forever
+  yesCameraAccess //Yay
+}
