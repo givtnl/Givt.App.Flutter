4e41ffdd3e1d7e5138897f3721a351d3644ee0b1
George Kavanagh <george@givtapp.net>
2022-10-10 13:39:04 +0200

    added a progress bar to intro screen and cleant up code

--- lib/features/startup/pages/startup_page.dart
+++ lib/features/startup/pages/startup_page.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:story_progress/story_progress.dart';
+import '../../../core/templates/logo_header_template.dart';
+
 import '../widgets/slide_header1.dart';
 import '../widgets/slide_header2.dart';
 import '../widgets/slide_header3.dart';
@@ -20,8 +23,10 @@ class _StartupPageState extends State<StartupPage> {
   final ValueNotifier<String> _notifierSVG =
       ValueNotifier('assets/svg/intro_background1.svg');
   final ValueNotifier<int> _notifierCurrentPage = ValueNotifier(0);
-  final Color _mainBackgroundColor = Color(0xFFF5F5F5);
+  final Color _mainBackgroundColor = const Color(0xFFF5F5F5);
+  final Duration _slideDuration = const Duration(seconds: 3);
   late final _carouselController = CarouselController();
+  final _storyProgressKey = GlobalKey<StoryProgressState>();
   int _currentPage = 0;
   bool _isExpanded = false;
 
@@ -34,7 +39,9 @@ class _StartupPageState extends State<StartupPage> {
   @override
   void initState() {
     super.initState();
-    startCarousel();
+    Future.delayed(Duration.zero, () {
+      startCarousel();
+    });
   }
 
   @override
@@ -43,7 +50,12 @@ class _StartupPageState extends State<StartupPage> {
   }
 
   void startCarousel() {
-    Timer.periodic(const Duration(seconds: 4), (timer) {
+    print(_storyProgressKey.currentState);
+    if (_storyProgressKey.currentState?.isPlaying() == false) {
+      _storyProgressKey.currentState?.resume();
+    }
+
+    Timer.periodic(_slideDuration, (timer) {
       _currentPage++;
 
       if (_currentPage == 3) {
@@ -86,126 +98,148 @@ class _StartupPageState extends State<StartupPage> {
         body: Container(
       color: _mainBackgroundColor,
       child: Stack(children: <Widget>[
-        ValueListenableBuilder(
-          valueListenable: _notifierCurrentPage,
-          builder: (BuildContext context, int val, Widget? child) {
-            return Center(
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(50)),
-                child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 800),
-                  color: (val == 2)
-                      ? _isExpanded
-                          ? const Color.fromARGB(255, 248, 232, 181)
-                          : const Color(0xFFEDC646)
-                      : _mainBackgroundColor,
-                  padding: const EdgeInsets.only(top: 20),
-                  width:
-                      _isExpanded ? MediaQuery.of(context).size.height * 1 : 60,
-                  height:
-                      _isExpanded ? MediaQuery.of(context).size.height * 1 : 60,
-                  onEnd: animationComplete,
-                ),
-              ),
-            );
-          },
-        ),
+        _buildCircleExpansion(),
         Column(
           children: [
             SafeArea(
               child: AnimatedOpacity(
-                opacity: _isExpanded ? 0.0 : 1.0,
-                duration: const Duration(milliseconds: 500),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-                  child: SvgPicture.asset(
-                    'assets/svg/logo.svg',
-                    height: 25,
-                  ),
-                ),
-              ),
+                  opacity: _isExpanded ? 0.0 : 1.0,
+                  duration: const Duration(milliseconds: 500),
+                  child: const LogoHeaderTemplate()),
             ),
             Stack(
               children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(bottom: 100),
-                  height: MediaQuery.of(context).size.height * 0.8,
-                  width: double.infinity,
-                  child: AnimatedOpacity(
-                    opacity: _isExpanded ? 1.0 : 0.0,
-                    duration: const Duration(milliseconds: 500),
-                    child: const Center(
-                      child: Text(
-                        'Welcome to givt',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                AnimatedOpacity(
-                  opacity: _isExpanded ? 0.0 : 1.0,
-                  duration: const Duration(milliseconds: 500),
-                  child: ValueListenableBuilder(
-                      valueListenable: _notifierSVG,
-                      builder:
-                          (BuildContext context, String svg, Widget? child) {
-                        return Container(
-                          // color: Colors.yellow,
-                          padding: const EdgeInsets.only(top: 330),
-                          child: Center(
-                              child: SvgPicture.asset(
-                            svg,
-                          )),
-                        );
-                      }),
-                ),
-                AnimatedOpacity(
-                  opacity: _isExpanded ? 0.0 : 1.0,
-                  duration: const Duration(milliseconds: 500),
-                  child: CarouselSlider(
-                      carouselController: _carouselController,
-                      options: CarouselOptions(
-                          height: MediaQuery.of(context).size.height * 0.8,
-                          enableInfiniteScroll: false,
-                          viewportFraction: 1,
-                          onPageChanged: (integer, _) =>
-                              {setOvalBackgroundShape(integer)}),
-                      items: svgImages.map((svg) {
-                        return ValueListenableBuilder(
-                          valueListenable: _notifierCurrentPage,
-                          builder:
-                              (BuildContext context, int val, Widget? child) {
-                            return Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Container(
-                                  child: (val == 0)
-                                      ? const SlideHeader1()
-                                      : (val == 1)
-                                          ? const SlideHeader2()
-                                          : (val == 2)
-                                              ? SlideHeader3()
-                                              : Container(),
-                                ),
-                                Container(
-                                    margin: const EdgeInsets.only(bottom: 40),
-                                    child: SvgPicture.asset(svg, width: 320)),
-                              ],
-                            );
-                          },
-                        );
-                      }).toList()),
-                ),
+                _buildWelcomeTransition(),
+                _buildBackgroundBlob(),
+                _buildCarousel()
               ],
             ),
           ],
         ),
       ]),
     ));
+  }
+
+  Widget _buildCircleExpansion() {
+    return ValueListenableBuilder(
+      valueListenable: _notifierCurrentPage,
+      builder: (BuildContext context, int val, Widget? child) {
+        return Center(
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(50)),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 800),
+              color: (val == 2)
+                  ? _isExpanded
+                      ? const Color.fromARGB(255, 248, 232, 181)
+                      : const Color(0xFFEDC646)
+                  : _mainBackgroundColor,
+              padding: const EdgeInsets.only(top: 20),
+              width: _isExpanded ? MediaQuery.of(context).size.height * 1 : 60,
+              height: _isExpanded ? MediaQuery.of(context).size.height * 1 : 60,
+              onEnd: animationComplete,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildWelcomeTransition() {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 100),
+      height: MediaQuery.of(context).size.height * 0.8,
+      width: double.infinity,
+      child: AnimatedOpacity(
+        opacity: _isExpanded ? 1.0 : 0.0,
+        duration: const Duration(milliseconds: 500),
+        child: const Center(
+          child: Text(
+            'Welcome to givt',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackgroundBlob() {
+    return AnimatedOpacity(
+      opacity: _isExpanded ? 0.0 : 1.0,
+      duration: const Duration(milliseconds: 500),
+      child: ValueListenableBuilder(
+          valueListenable: _notifierSVG,
+          builder: (BuildContext context, String svg, Widget? child) {
+            return Container(
+              padding: const EdgeInsets.only(top: 260),
+              child: Center(
+                  child: SvgPicture.asset(
+                svg,
+              )),
+            );
+          }),
+    );
+  }
+
+  Widget _buildCarousel() {
+    return AnimatedOpacity(
+      opacity: _isExpanded ? 0.0 : 1.0,
+      duration: const Duration(milliseconds: 500),
+      child: Column(
+        children: [
+          CarouselSlider(
+            carouselController: _carouselController,
+            options: CarouselOptions(
+                height: MediaQuery.of(context).size.height * 0.7,
+                enableInfiniteScroll: false,
+                viewportFraction: 1,
+                onPageChanged: (integer, _) =>
+                    {setOvalBackgroundShape(integer)}),
+            items: svgImages.map((svg) {
+              return ValueListenableBuilder(
+                valueListenable: _notifierCurrentPage,
+                builder: (BuildContext context, int val, Widget? child) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        child: (val == 0)
+                            ? const SlideHeader1()
+                            : (val == 1)
+                                ? const SlideHeader2()
+                                : (val == 2)
+                                    ? SlideHeader3()
+                                    : Container(),
+                      ),
+                      Container(
+                          margin: const EdgeInsets.only(bottom: 40),
+                          child: SvgPicture.asset(svg, width: 320)),
+                    ],
+                  );
+                },
+              );
+            }).toList(),
+          ),
+          _buildSliderProgressBars()
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSliderProgressBars() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(10, 50, 10, 10),
+      child: StoryProgress(
+        key: _storyProgressKey,
+        progressCount: 3,
+        duration: _slideDuration,
+        onStatusChanged: (_) {},
+      ),
+    );
   }
 }
