5b5444a571c40d22dfd3c3b3bc030e3c6fc4723a
Daniela510 <whateverellie@gmail.com>
2022-10-12 10:57:32 +0200

    user as separate entity in local model

--- lib/features/basic_giving_flow/controller/create_cachedGivt.dart
+++ lib/features/basic_giving_flow/controller/create_cachedGivt.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:givt_mobile_apps/models/localStorage.dart';
 
 import '../../../services/navigation_service.dart';
@@ -8,11 +9,16 @@ void createCachedGivtandNavigate(int donationAmount, String mediumId) {
   DateTime dateTime = DateTime.now();
 
   final NavigationService navigationService = locator<NavigationService>();
-  final LocalUserProxy model = locator<LocalUserProxy>();
+  final LocalStorageProxy model = locator<LocalStorageProxy>();
 
 // fourth arg is null bc the current flow doesnt register the email so it does not fetch the userId
   model.createCachedGivt(mediumId, donationAmount, dateTime, null);
 
-// this should navigate to the iframescreen
+// check for connection before this navigation
+  if (Platform.isIOS) {
+    // this should direct to ifram in the inapp browser bc of apple store policies
+    navigationService.navigateTo(routes.DonationSuccessRoute);
+  }
+// this should direct to iframe in the app
   navigationService.navigateTo(routes.DonationSuccessRoute);
 }
--- lib/models/localStorage.dart
+++ lib/models/localStorage.dart
@@ -4,59 +4,68 @@ import 'package:realm/realm.dart';
 part 'localStorage.g.dart';
 
 @RealmModel()
-class _LocalUser {
+class _LocalStorage {
   bool locationAsked = false;
   bool cameraAsked = false;
-  // non_constant_identifier_names
-  String? email;
-  String? userId;
-  // should the iban be in local storage ?
-  String? iBAN;
-  String? phoneNumber;
-  String? firstName;
-  String? lastName;
-  String? address;
-  String? city;
-  String? postalCode;
-  String? country;
-  //no password in local storage haha
-  int? amountLimit;
-  String? appLanguage;
-  String? timeZoneId;
+  late _LocalUser? userData;
   // maybe this should take an array/list of type CachedGivts
   List<_CachedGivts> cachedGivts = [];
 }
 
 @RealmModel()
+class _LocalUser {
+  // non_constant_identifier_names
+  String email = "";
+  String userId = "";
+  String iBAN = "";
+  String phoneNumber = "";
+  String firstName = "";
+  String lastName = "";
+  String address = "";
+  String city = "";
+  String postalCode = "";
+  String country = "";
+  int amountLimit = 10000;
+  String appLanguage = "";
+  String timeZoneId = "";
+}
+
+@RealmModel()
 class _CachedGivts {
   // does it need to store more data?
-  // it definetely needs to store some id/ token
+  // it SEEMS TO needs to store some id/ token
   late final String mediumId;
   late final int donationAmount;
+  // Is this just dateTime.now()???
   late final DateTime dateTime;
   late final String? userId;
+  // should this then have a token?
 }
 
 /// A proxy of the data
 /// should this be a separate file? i like it here but maybeee
-class LocalUserProxy {
+class LocalStorageProxy {
   late Realm realm;
 
-  LocalUserProxy() {
-    var config = Configuration.local([LocalUser.schema, CachedGivts.schema]);
+  LocalStorageProxy() {
+    var config = Configuration.local(
+        [LocalStorage.schema, CachedGivts.schema, LocalUser.schema]);
     realm = Realm(config);
 
-    if (realm.all<LocalUser>().isEmpty) {
-      realm.write(() => realm.add(LocalUser(
-            locationAsked: false,
-            cameraAsked: false,
-          )));
+    if (realm.all<LocalStorage>().isEmpty) {
+      realm.write(() {
+        realm.add(LocalStorage(
+          locationAsked: false,
+          cameraAsked: false,
+        ));
+        realm.all<LocalStorage>().first.userData = LocalUser();
+      });
     }
   }
 
   void createCachedGivt(mediumId, donationAmount, dateTime, givenUserId) {
     realm.write(() {
-      LocalUser localCurrent = realm.all<LocalUser>().first;
+      LocalStorage localCurrent = realm.all<LocalStorage>().first;
       localCurrent.cachedGivts
           .add(CachedGivts(mediumId, donationAmount, dateTime, givenUserId));
     });
@@ -78,8 +87,7 @@ class LocalUserProxy {
     timeZoneId,
   ) {
     realm.write(() {
-      LocalUser localCurrent = realm.all<LocalUser>().first;
-      localCurrent.email = email;
+      LocalUser localCurrent = realm.all<LocalStorage>().first.userData!;
       localCurrent.userId = userId;
       localCurrent.iBAN = iBAN;
       localCurrent.phoneNumber = phoneNumber;
@@ -97,7 +105,7 @@ class LocalUserProxy {
 
   void updateProgress(String key) {
     realm.write(() {
-      LocalUser localCurrent = realm.all<LocalUser>().first;
+      LocalStorage localCurrent = realm.all<LocalStorage>().first;
       if (key == 'camera') {
         localCurrent.cameraAsked = true;
       }
@@ -110,7 +118,7 @@ class LocalUserProxy {
   // this is utility for resetting the flow
   void downgradeProgress(String key) {
     realm.write(() {
-      LocalUser localCurrent = realm.all<LocalUser>().first;
+      LocalStorage localCurrent = realm.all<LocalStorage>().first;
       if (key == 'camera') {
         localCurrent.cameraAsked = false;
       }
