ecabe7cb182ab3cf9df9e70d94ccec4e62fd28ef
Daniela510 <whateverellie@gmail.com>
2022-10-12 11:50:15 +0200

    less duplication

--- lib/features/basic_giving_flow/controller/typical_donation_controller.dart
+++ lib/features/basic_giving_flow/controller/typical_donation_controller.dart
@@ -1,4 +1,4 @@
-bool selectedMin = true;
+bool selectedMin = false;
 bool selectedMed = false;
 bool selectedMax = false;
 bool differentChoice = false;
@@ -7,35 +7,20 @@ Map<String, dynamic> results = {};
 
 getAmount(int amount, bool userInput, Map<String, dynamic> previous) {
   if (userInput == false && amount == previous['donationAmount']) {
-    selectedMin = false;
-    selectedMed = false;
-    selectedMax = false;
-    donationAmount = 0;
+    return;
   } else {
     switch (amount) {
       case 5:
         selectedMin = true;
-        selectedMed = false;
-        selectedMax = false;
         donationAmount = 5;
         break;
       case 10:
-        selectedMin = false;
         selectedMed = true;
-        selectedMax = false;
         donationAmount = 10;
         break;
       case 15:
-        selectedMin = false;
-        selectedMed = false;
         selectedMax = true;
         donationAmount = 15;
-        break;
-      default:
-        selectedMin = false;
-        selectedMed = false;
-        selectedMax = false;
-        donationAmount = amount;
         break;
     }
   }
@@ -43,7 +28,6 @@ getAmount(int amount, bool userInput, Map<String, dynamic> previous) {
     "selectedMin": selectedMin,
     "selectedMed": selectedMed,
     "selectedMax": selectedMax,
-    "userInput": userInput,
     "donationAmount": donationAmount
   };
   return results;
