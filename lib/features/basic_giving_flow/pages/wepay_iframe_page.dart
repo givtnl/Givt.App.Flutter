5792a1212ab5fa8f5c58c8747052c2f12711ce5d
Mike Pattyn <mike.pattyn1@gmail.com>
2022-10-13 12:22:45 +0200

    clean up page

--- lib/features/basic_giving_flow/pages/wepay_iframe_page.dart
+++ lib/features/basic_giving_flow/pages/wepay_iframe_page.dart
@@ -1,6 +1,3 @@
-import 'dart:collection';
-
-import 'package:flutter/services.dart';
 import 'package:givt_mobile_apps/models/html.dart';
 import 'package:logger/logger.dart';
 import 'package:flutter/material.dart';
@@ -38,9 +35,11 @@ class _WePayPageState extends State<WePayPage> {
               height: 150,
               child: InAppWebView(
                 initialOptions: InAppWebViewGroupOptions(
-                  android: AndroidInAppWebViewOptions(textZoom: 0),
+                  android: AndroidInAppWebViewOptions(),
                   crossPlatform: InAppWebViewOptions(
-                      javaScriptEnabled: true, disableVerticalScroll: true),
+                    javaScriptEnabled: true,
+                    disableVerticalScroll: true,
+                  ),
                 ),
                 // initialUrlRequest: URLRequest(
                 //   url: Uri(
@@ -60,140 +59,11 @@ class _WePayPageState extends State<WePayPage> {
             text: "Hi",
             disabled: false,
             onClicked: () => {
-              Tokenize(
-                webViewController!,
-              )
+              webViewController?.evaluateJavascript(source: "tokenize();"),
             },
           )
         ],
       ),
     );
   }
-
-  void ReloadWebView(InAppWebViewController controller) {
-    controller.removeJavaScriptHandler(handlerName: handlerName);
-  }
-
-  void LoadWebView(InAppWebViewController controller) {
-    controller.addJavaScriptHandler(
-        handlerName: handlerName,
-        callback: (args) => {
-              logger.wtf(args),
-            });
-    controller.loadData(
-      data: WepayHtml.body,
-    );
-  }
-
-  void PostMessageIframeLoaded(InAppWebViewController controller) {}
-  void Tokenize(InAppWebViewController controller) {
-    const String tokenizeJavascript = """
-        tokenize();
-    """;
-    controller.evaluateJavascript(source: tokenizeJavascript);
-  }
 }
-
-Future<JsBeforeUnloadResponse?> getResponse(request) async {
-  logger.i(request);
-  return null;
-}
-
-// import 'dart:async';
-
-// import 'package:flutter/material.dart';
-// import 'package:givt_mobile_apps/core/widgets/buttons/generic_button.dart';
-// import 'dart:io';
-// import 'package:webview_flutter/webview_flutter.dart';
-// import '../../../core/templates/logo_header_template.dart';
-// import '../../../core/widgets/buttons/generic_button.dart';
-
-// class WePayPage extends StatefulWidget {
-//   WePayPage({super.key});
-
-//   @override
-//   State<WePayPage> createState() => _WePayPageState();
-// }
-
-// class _WePayPageState extends State<WePayPage> {
-//   final Completer<WebViewController> _controller =
-//       Completer<WebViewController>();
-
-//   @override
-//   void initState() {
-//     if (Platform.isAndroid) {
-//       WebView.platform = SurfaceAndroidWebView();
-//     } else if (Platform.isIOS) {
-//       WebView.platform = CupertinoWebView();
-//     }
-//     ;
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         //color: Colors.green,
-//         //width: double.infinity,
-//         //height: MediaQuery.of(context).size.height * 0.7,
-//         child: Column(
-//           children: <Widget>[
-//             const LogoHeaderTemplate(),
-//             SizedBox(
-//               width: double.infinity,
-//               height: MediaQuery.of(context).size.height * 0.3,
-//               child: WebView(
-//                 javascriptMode: JavascriptMode.unrestricted,
-//                 initialUrl:
-//                     'https://givt-debug-api.azurewebsites.net/wepay-flutter.html',
-//                 onWebViewCreated: _controller.complete,
-//                 onWebResourceError: _controller.completeError,
-//                 javascriptChannels: Set.from([
-//                   JavascriptChannel(
-//                       name: 'Print',
-//                       onMessageReceived: (JavascriptMessage message) {
-//                         //This is where you receive message from
-//                         //javascript code and handle in Flutter/Dart
-//                         //like here, the message is just being printed
-//                         //in Run/LogCat window of android studio
-//                         print(message.message);
-//                       })
-//                 ]),
-//               ),
-//             ),
-//             GenericButton(
-//               text: 'Submit',
-//               disabled: false,
-//               onClicked: () async {
-//                 final controller = await _controller.future;
-//                 print(controller);
-//                 //         var script = """
-//                 //     window.wkVars.messageHandler.postMessage({'event': 'Start tokenizing'})
-//                 //     const tokenize = window.wkVars.creditCard.tokenize()
-//                 //       .then(function(response) {
-//                 //         window.wkVars.messageHandler.postMessage({'token': response.id});
-//                 //       })
-//                 //       .catch(function(error) {
-//                 //         let key = error[0].target[0];
-//                 //         window.wkVars.creditCard.setFocus(key);
-//                 //         window.wkVars.messageHandler.postMessage({'error': JSON.stringify(error)});
-//                 //       });
-//                 // """;
-
-//                 var script = """
-//                               let creditCard = WePay.createCreditCardIframe(iframe_container_id, options);
-//                               Print.postMessage(JSON.stringify(creditCard))
-                             
-//                             """;
-//                 var response =
-//                     await controller.runJavascriptReturningResult(script);
-//                 print(response);
-//               },
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
