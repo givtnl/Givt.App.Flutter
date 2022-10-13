77dab8c6b3f6b51f8226b372cfcb57f2f88d15e7
Daniela510 <whateverellie@gmail.com>
2022-10-13 11:41:00 +0200

    check for connection

--- lib/features/basic_giving_flow/controller/create_cachedGivt.dart
+++ lib/features/basic_giving_flow/controller/create_cachedGivt.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 import 'package:givt_mobile_apps/models/localStorage.dart';
+import 'package:givt_mobile_apps/utils/connection_check.dart';
 
 import '../../../services/navigation_service.dart';
 import '../../../utils/locator.dart';
 import '../../../core/constants/route_paths.dart' as routes;
 
-void createCachedGivtandNavigate(int donationAmount, String mediumId) {
+Future<void> createCachedGivtandNavigate(
+    int donationAmount, String mediumId) async {
+  bool connected = await tryConnection();
   DateTime dateTime = DateTime.now();
 
   final NavigationService navigationService = locator<NavigationService>();
@@ -14,11 +17,13 @@ void createCachedGivtandNavigate(int donationAmount, String mediumId) {
 // fourth arg is null bc the current flow doesnt register the email so it does not fetch the userId
   model.createCachedGivt(mediumId, donationAmount, dateTime, null);
 
-// check for connection before this navigation
-  if (Platform.isIOS) {
+  if (connected && Platform.isIOS) {
     // this should direct to ifram in the inapp browser bc of apple store policies
     navigationService.navigateTo(routes.DonationSuccessRoute);
-  }
+  } else if (connected) {
 // this should direct to iframe in the app
-  navigationService.navigateTo(routes.DonationSuccessRoute);
+    navigationService.navigateTo(routes.DonationSuccessRoute);
+  } else {
+    // sorry you cannot make a donnation while offline
+  }
 }
