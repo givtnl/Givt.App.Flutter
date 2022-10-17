20e00339d4955c585c95b416a160da6e8cbce78f
George Kavanagh <george@givtapp.net>
2022-10-17 15:53:15 +0200

    undid changes to splashscreen

--- lib/features/splash_screen/pages/splash_screen_page.dart
+++ lib/features/splash_screen/pages/splash_screen_page.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
-import 'dart:async';
 import '../../startup/pages/startup_page.dart';
 import '../../../services/navigation_service.dart';
 import '../../../utils/locator.dart';
-import '../controllers/splash_screen_controller.dart';
 
 class SplashScreenPage extends StatelessWidget {
-  NavigationService _navigationService = locator<NavigationService>();
+  final NavigationService _navigationService = locator<NavigationService>();
   SplashScreenPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
-      navigateAfterFuture:
-          SplashScreenController(context).createTempUserAndNavigate(),
+      seconds: 5,
       image: Image.asset('assets/images/logo.png'),
       backgroundColor: Colors.white,
       photoSize: 200.0,
       loaderColor: Theme.of(context).colorScheme.secondary,
+      navigateAfterSeconds: const StartupPage(),
     );
   }
 }
