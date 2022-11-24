1443dbabdbd5955cfebd839089eef76a2296e998
daniela <whateverellie@gmail.com>
2022-11-24 15:05:50 +0100

    assurance

--- lib/utils/locator.dart
+++ lib/utils/locator.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:givt_mobile_apps/services/camera_service.dart';
 import 'package:givt_mobile_apps/services/local_donation_service.dart';
+import 'package:givt_mobile_apps/services/local_storage_service.dart';
 import 'package:givt_mobile_apps/services/local_user_service.dart';
 import 'package:givt_mobile_apps/services/location_service.dart';
 import 'package:givt_mobile_apps/services/persistent_state_service.dart';
@@ -12,6 +13,7 @@ GetIt locator = GetIt.instance;
 void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
   locator.registerLazySingleton(() => UserService());
+  locator.registerLazySingleton(() => LocalStorageBase());
   locator.registerLazySingleton(() => PersistentStateService());
   locator.registerLazySingleton(() => LocalDonationService());
   locator.registerLazySingleton(() => LocalUserService());
