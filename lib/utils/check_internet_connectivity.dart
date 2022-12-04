dee2fce7123614d67038df589b065fba2210d21c
daniela <whateverellie@gmail.com>
2022-12-04 19:20:26 +0100

    one time connectivity check method

--- lib/utils/check_internet_connectivity.dart
+++ lib/utils/check_internet_connectivity.dart
@@ -6,6 +6,16 @@ class CheckInternet extends ChangeNotifier {
   bool hasInternet = true;
   final Connectivity _connectivity = Connectivity();
   late StreamSubscription _streamSubscription;
+  Future<void> checkInitialConnection() async {
+    final status = await _connectivity.checkConnectivity();
+    if (status == ConnectivityResult.none) {
+      hasInternet = false;
+      notifyListeners();
+    } else {
+      hasInternet = true;
+      notifyListeners();
+    }
+  }
 
   void checkRealtimeConnection() {
     _streamSubscription = _connectivity.onConnectivityChanged.listen((event) {
