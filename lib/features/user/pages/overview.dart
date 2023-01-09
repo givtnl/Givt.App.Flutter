d229d285bd34ae0c2f6c4dfa21a2850f2535751e
Alex Bejan <alex_bejan11@yahoo.com>
2023-01-10 00:11:04 +0100

    DP-180 Implemente initial overview screen, WIP

--- lib/features/user/pages/overview.dart
+++ lib/features/user/pages/overview.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:givt_mobile_apps/core/templates/logo_header_template.dart';
+import 'package:givt_mobile_apps/core/widgets/notifications/no_connection_bar.dart';
+import 'package:givt_mobile_apps/services/check_internet_connectivity.dart';
+import 'package:provider/provider.dart';
 
 class OverviewPage extends StatefulWidget {
   const OverviewPage({Key? key}) : super(key: key);
@@ -10,6 +14,38 @@ class OverviewPage extends StatefulWidget {
 class _OverviewPageState extends State<OverviewPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Consumer<ConnectivityService>(
+          builder: (context, provider, child) {
+            return Stack(
+              children: [
+                child!,
+                (!provider.hasInternet)
+                    ? const noConnectionBar()
+                    : const SizedBox(),
+              ],
+            );
+          },
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const LogoHeaderTemplate(),
+                  const SizedBox(height: 25),
+                  Text(
+                    'Thank you for your generous generousity!',
+                    style: Theme.of(context).textTheme.headline6?.copyWith(
+                        fontWeight: FontWeight.w800,
+                        color: Theme.of(context).colorScheme.surface),
+                  ),
+                  const SizedBox(height: 15),
+                ],
+              ),
+            ),
+          )),
+    );
   }
 }
