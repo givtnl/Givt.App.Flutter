6906680af04ccf355c6a4f6cd9e635bed216330c
Alex Bejan <alex_bejan11@yahoo.com>
2023-01-09 22:09:06 +0100

    DP-168 Added Interceptor and retry policy to api service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,8 +4,9 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:givt_mobile_apps/models/temp_user.dart';
 import 'package:givt_mobile_apps/services/local_storage_service.dart';
-import 'package:http/http.dart' as http;
-import '../core/constants/environment_variables.dart';
+import 'package:givt_mobile_apps/utils/api_service_interceptor.dart';
+import 'package:http/http.dart';
+import 'package:http_interceptor/http/http.dart';
 import '../models/local_storage.dart';
 import '../models/registered_user.dart';
 import '../utils/locator.dart';
@@ -15,28 +16,32 @@ var logger = Logger(
   printer: PrettyPrinter(),
 );
 
+/// [ApiService] is a wrapper around the [Client]
 class APIService {
+  Client client = InterceptedClient.build(
+    interceptors: [
+      GivtApiInterceptor(),
+    ],
+    retryPolicy: ExpiredTokenRetryPolicy(),
+  );
+
   late final LocalStorageBase storageProxy = locator<LocalStorageBase>();
   String apiURL =
       kReleaseMode ? 'api.givt.app' : 'givt-debug-api.azurewebsites.net';
   APIService() {
     logger.wtf('api url is ${apiURL}');
   }
-  Map<String, String> get headers => {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      };
 
   Future<bool> checktld(String email) async {
     final url = Uri.https(apiURL, '/api/checktld', {'email': email});
-    final response = await http.get(url);
+    final response = await client.get(url);
     return (response.statusCode >= 400) ? false : true;
   }
 
   Future<String> createTempUser(TempUser user) async {
     final encodedUser = jsonEncode(user);
     final url = Uri.https(apiURL, '/api/v2/users');
-    var response = await http.post(url, body: encodedUser, headers: headers);
+    var response = await client.post(url, body: encodedUser);
     // response.body is plain text of the user ID or an error
     if (response.statusCode >= 400) {
       throw HttpException(response.body);
@@ -48,7 +53,7 @@ class APIService {
   Future<dynamic> createRegisteredUser(RegisteredUser user) async {
     final encodedUser = jsonEncode(user);
     final url = Uri.https(apiURL, '/api/v2/users/register');
-    var response = await http.post(url, body: encodedUser, headers: headers);
+    var response = await client.post(url, body: encodedUser);
     if (response.statusCode >= 400) {
       throw Exception('Failed to create a registered user');
     } else {
@@ -63,7 +68,7 @@ class APIService {
       'paymentMethodToken': wepayToken,
       'userId': userId,
     });
-    var response = await http.post(url, body: mandate, headers: headers);
+    var response = await client.post(url, body: mandate);
     if (response.statusCode >= 400) {
       throw Exception('Failed to create a mandate for user: $userId.');
     } else {
@@ -75,8 +80,7 @@ class APIService {
   Future<dynamic> submitDonation(String userId, String wepayToken) async {
     final encodedDonation = getEncodedDonation(wepayToken);
     final url = Uri.https(apiURL, '/api/v2/users/$userId/givts');
-    var response =
-        await http.post(url, body: encodedDonation, headers: headers);
+    var response = await client.post(url, body: encodedDonation);
     if (response.statusCode >= 400) {
       throw Exception('Failed to process donation');
     } else {
@@ -115,7 +119,7 @@ class APIService {
 
   Future<dynamic> checkEmailExists(String email) async {
     final url = Uri.https(apiURL, '/api/v2/Users/check', {'email': email});
-    var response = await http.get(url, headers: headers);
+    var response = await client.get(url);
     if (response.statusCode >= 400) {
       throw Exception('Failed to check email');
     } else {
@@ -124,11 +128,12 @@ class APIService {
   }
 
   Future<dynamic> login(Map loginCredentials) async {
-    final url = Uri.https(apiURL, '/oauth2/token');
+    final url = Uri.https(apiURL, '/api/Givts',
+        {'UserId': '698c3dcf-8083-49e3-a80d-d6591d0a41c7'});
     // might need to use api/v2/users/login
-    var response = await http.post(
+    var response = await client.get(
       url,
-      body: loginCredentials,
+      // body: loginCredentials,
     );
     if (response.statusCode >= 400) {
       throw Exception(jsonDecode(response.body));
@@ -139,7 +144,21 @@ class APIService {
 
   Future<dynamic> getOrganisationDetailsFromMedium(String mediumCode) async {
     final url = Uri.https(apiURL, '/api/v3/campaigns', {'code': mediumCode});
-    var response = await http.get(url, headers: headers);
+    var response = await client.get(url);
+    if (response.statusCode >= 400) {
+      throw Exception('something went wrong :(');
+    } else {
+      return response.body;
+    }
+  }
+
+  /// Returns the new access token with a new refresh_token
+  Future<dynamic> refreshToken(String refreshToken) async {
+    final url = Uri.https(apiURL, '/oauth2/token');
+    var response = await client.post(url, body: {
+      'grant_type': 'refresh_token',
+      'refresh_token': refreshToken,
+    });
     if (response.statusCode >= 400) {
       throw Exception('something went wrong :(');
     } else {
